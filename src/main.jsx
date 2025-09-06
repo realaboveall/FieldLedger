450a3a4deb3bc7834a019d8511f61be879a5dd71
Atulya Rounak <84398957+realaboveall@users.noreply.github.com>
2025-09-06 21:31:29 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import Home from "./Home.jsx";
 import Problem from "./Problem.jsx";
-import Chatbox from "./ChatBox.jsx";
+import Chatbox from "./Chatbox.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
