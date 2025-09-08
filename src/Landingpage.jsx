0effcdb7bdac6c4c41c6fd291efaafe238506112
Abhay Sharma <abhaysharma.mrt@gmail.com>
2025-09-09 01:48:48 +0530

    really merged

--- src/Landingpage.jsx
+++ src/Landingpage.jsx
@@ -3,13 +3,19 @@ import Home from "./Home.jsx";
 import Problem from "./Problem.jsx";
 import Chatbox from "./Chatbox.jsx";
 import React from 'react'
+import ScrollAnimationSection from "./ScrollAnimationSection.jsx";
+import Navbar from "./Navbar.jsx";
+import ScrollAnimationSectionNodes from "./ScrollAnimationSectionNodes.jsx";
 
-export default function Landingpage () {
+export default function Landingpage() {
     return (
         <div>
+
+            {/* <Navbar /> */}
+            <App />
             <Home />
             <Chatbox />
-            <App />
+            <ScrollAnimationSectionNodes />
             {/* <Problem /> */}
         </div>
     )
